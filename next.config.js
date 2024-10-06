a0744affdde36de562f7e01220b1708a845573fc
Yannick MATH <yannick_math@yahoo.fr>
2024-10-06 17:03:46 +0200

    general review

--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.scdn.co'],
+    domains: ["i.scdn.co"], // Domaine pour les images autorisées
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Appliquer à toutes les pages
+        headers: [
+          { key: "Cache-Control", value: "no-cache" }, // Cache control pour toutes les pages
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
